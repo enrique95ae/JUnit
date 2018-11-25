d57d38b5e087c90cb3c8f48e036abc600b0adf2e
enrique95ae <enrique95ae@gmail.com>
2018-11-24 17:42:52 -0800

    Initial commit

---
+++ Tetris JUnit TestCases/Tetris-master/test/org/psnbtech/BoardPanelTest.java
@@ -0,0 +1,36 @@
+package org.psnbtech;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardPanelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIsValidAndEmpty() {
+
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPiece() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckLines() {
+		fail("Not yet implemented");
+	}
+
+}
